0959d22154d419d154609ddafe554d0a36f199a9
Joy Tempkins <jtempkins@gmail.com>
2019-08-30 16:04:54 -0500

    linked images, all buttons not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 var topics = ["spongebob", "simpsons", "rugrats", "garfield", "jetsons"]
 
+
 function createButtons() {
 
     // Deleting the cartoon buttons before adding new buttons so no repeats
@@ -17,7 +18,7 @@ function createButtons() {
     }
   }
 
-//  Event handler when a button is clicked
+//  Event handler when a new cartoon is added
     $("#addCartoon").on("click", function(event) {
     event.preventDefault();
 
@@ -33,6 +34,47 @@ function createButtons() {
   createButtons();
 
 
-  // ISSUES: input form is not clearing
 
+//attempt to add images from here:
 
+  // //  // Event listener for  topic buttons
+   $("button").on("click", function() {
+
+   var toon = $(this).attr("cartoon");
+  
+   //   // // Storing our giphy API URL for a random cat image
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=LbW049mOxR1yjz75yNYMbx9Ufj1FxX32&limit=10";
+
+  //   // // // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+    // //   // After the data from the AJAX request comes back
+      .then(function(response) {
+      
+        var results = response.data;
+        for(var i = 0; i < results.length; i++){
+          var cartoonDiv = $("<div>");
+          var p = $("<p>").text("Rating: " + results[i].rating);
+          var toonImage = $("<img>");
+          toonImage.attr("src", results[i].images.fixed_height.url);
+          cartoonDiv.append(p);
+          cartoonDiv.append(toonImage);
+          $("#CartoonHolder").prepend(cartoonDiv);
+        }
+
+    // //     // Saving the image_original_url property
+    //     var imageUrl = response.data.image_original_url;
+
+    // //     // Creating and storing an image tag
+    //     var toonImage = $("<img>");
+
+    // //     // Setting the catImage src attribute to imageUrl
+    //     toonImage.attr("src", imageUrl);
+    //     toonImage.attr("alt", "cartoon image");
+
+    // //     // Prepending the catImage to the images div
+    //     $("#images").prepend(toonImage);
+   })});
