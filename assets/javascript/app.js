87f7f9ea00eee412d2ee0d2b2d5c655a61bb7c66
Joy Tempkins <jtempkins@gmail.com>
2019-08-31 10:56:17 -0500

    cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,76 +3,82 @@ var topics = ["spongebob", "simpsons", "rugrats", "garfield", "jetsons"]
 
 function createButtons() {
 
-    // Deleting the cartoon buttons before adding new buttons so no repeats
-    $("#buttonsClear").empty();
+  // Deleting the cartoon buttons before adding new buttons so no repeats
+  $("#buttonsClear").empty();
 
-    // Looping through the array of topics
-    for (var i = 0; i < topics.length; i++) {
+  // Looping through the array of topics
+  for (var i = 0; i < topics.length; i++) {
 
-      // Then dynamicaly generating buttons for each cartoon in the array.
-      var a = $("<button>");
-      a.addClass("cartoon");
-      a.attr("data-name", topics[i]);
-      a.text(topics[i]);
-      $("#cartoonHolder").append(a);
-    }
+    // Then dynamicaly generating buttons for each cartoon in the array.
+    var a = $("<button>");
+    a.addClass("cartoon");
+    a.attr("data-name", topics[i]);
+    a.text(topics[i]);
+    $("#cartoonHolder").append(a);
   }
+}
 
 //  Event handler when a new cartoon is added
-    $("#addCartoon").on("click", function(event) {
-    event.preventDefault();
+$("#addCartoon").on("click", function (event) {
+  event.preventDefault();
 
-    var cartoon = $("#cartoonInput").val().trim();
-    topics.push(cartoon);
+  var cartoon = $("#cartoonInput").val().trim();
+  topics.push(cartoon);
 
-    // Calling createButtons
-    createButtons();
-    $("#cartoonInput").val("");
-  });
-
-  // Calling the createButtons to display the initial list of cartoons
+  // Calling createButtons
   createButtons();
+  $("#cartoonInput").val("");
+});
 
-  // //  // Event listener for  topic buttons
-   $("#cartoonHolder").on("click", "button", function(event) {
-    event.preventDefault();
-   var toon = $(this).attr("data-name");
-    console.log(toon);
+// Calling the createButtons to display the initial list of cartoons
+createButtons();
 
-   //   // // Storing our giphy API URL for a random cat image
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + toon + "&api_key=LbW049mOxR1yjz75yNYMbx9Ufj1FxX32&limit=10";
+$("#imgContainer").on("click", "img", function (event) {
+  var dataState = $(this).attr("data-state")
+  if (dataState === "animate") {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+
+  else {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  }
+})
+
+
+// //  // Event listener for  topic buttons
+$("#cartoonHolder").on("click", "button", function (event) {
+  event.preventDefault();
+  var toon = $(this).attr("data-name");
+  console.log(toon);
+
+  //   // // Storing our giphy API URL for a random cat image
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + toon + "&api_key=LbW049mOxR1yjz75yNYMbx9Ufj1FxX32&limit=10";
 
   //   // // // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
 
     // //   // After the data from the AJAX request comes back
-      .then(function(response) {
-     console.log(response);
+    .then(function (response) {
+      console.log(response);
 
-        var results = response.data;
-        for(var i = 0; i < results.length; i++){
-          var cartoonDiv = $("<div>");
-          var p = $("<p>").text("Rating: " + results[i].rating);
-          var toonImage = $("<img>");
-          toonImage.attr("src", results[i].images.fixed_height.url);
-          cartoonDiv.append(p);
-          cartoonDiv.append(toonImage);
-          $("#imgContainer").prepend(cartoonDiv);
-        }
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var cartoonDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var toonImage = $("<img>");
+        toonImage.attr("src", results[i].images.fixed_height.url);
+        toonImage.attr("data-animate", results[i].images.fixed_height.url);
+        toonImage.attr("data-still", results[i].images.fixed_height_still.url);
+        toonImage.attr("data-state", "animate");
+        cartoonDiv.append(p);
+        cartoonDiv.append(toonImage);
+        $("#imgContainer").prepend(cartoonDiv);
+      }
 
-    // //     // Saving the image_original_url property
-    //     var imageUrl = response.data.image_original_url;
-
-    // //     // Creating and storing an image tag
-    //     var toonImage = $("<img>");
-
-    // //     // Setting the catImage src attribute to imageUrl
-    //     toonImage.attr("src", imageUrl);
-    //     toonImage.attr("alt", "cartoon image");
-
-    // //     // Prepending the catImage to the images div
-    //     $("#images").prepend(toonImage);
-   })});
+  
+})});
